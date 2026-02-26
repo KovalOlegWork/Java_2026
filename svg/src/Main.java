d8ab63c675940b9fac4963e9fb154c53f43ad067
Oleg Koval <KovalOlegWork@gmail.com>
2026-02-26 11:54:51 +0100

    svg z2

--- svg/src/Main.java
+++ svg/src/Main.java
@@ -1,6 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Point point = new Point();
+        point.x = 5.4F;
+        point.y = 2.3F;
+        System.out.println(point.toSvg());
     }
 }
 
